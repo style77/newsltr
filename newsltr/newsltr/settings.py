127090f8296909cb63fe88f8444ca00dd6969b6e
style77 <stylek777@gmail.com>
2023-09-27 20:09:46 +0200

    fix(api): add 0.0.0.0 to allowed hosts

--- newsltr/newsltr/settings.py
+++ newsltr/newsltr/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEVELOPMENT
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
