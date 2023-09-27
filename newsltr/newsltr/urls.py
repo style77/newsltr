36a7f334b913c944e51ab2d4a8a36d5e5ea4f1f2
style77 <stylek777@gmail.com>
2023-09-27 22:33:48 +0200

    feat(api): Openapi + bump schema

--- newsltr/newsltr/urls.py
+++ newsltr/newsltr/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-    path(r'api/v1/', include('authorization.urls'))
+    path(r'api/v1/', include('authorization.urls')),
+    path(r'api/v1/openapi', get_schema_view(
+        title="Newsltr",
+        description="Documentation for Newsltr API",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
