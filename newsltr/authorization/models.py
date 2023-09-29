ce2fa9bb3b4a609c92f0f88d3b49edbd3c1e93ba
style77 <stylek777@gmail.com>
2023-09-30 00:30:48 +0200

    chore(api): upade create_user function

--- newsltr/authorization/models.py
+++ newsltr/authorization/models.py
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.password = make_password(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -38,6 +38,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
