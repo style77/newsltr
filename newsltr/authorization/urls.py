bb506ac25121511c98e4dd885b6738c704c77c54
style77 <stylek777@gmail.com>
2023-09-29 15:38:13 +0200

    feat(api): custom jwt-create view

--- newsltr/authorization/urls.py
+++ newsltr/authorization/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 
+# Override the default djoser.urls.jwt and add my custom view for token obtain
+from authorization.views import EmailTokenObtainPairView
+from djoser.urls.jwt import urlpatterns as jwt_urlpatterns
+
+jwt_urlpatterns.pop(0)
+
 urlpatterns = [
     path(r"auth/", include("djoser.urls")),
     path(r"auth/", include("djoser.urls.jwt")),
     path(r"auth/", include("djoser.social.urls")),
+    path(r"auth/jwt/create/", EmailTokenObtainPairView.as_view(), name="jwt-create"),
 ]
