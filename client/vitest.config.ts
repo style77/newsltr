9f72b315390ad2d63f839d43c331fa6513b1d928
uKiJo <nes-you-ukijo@hotmail.fr>
2023-09-30 15:10:58 +0100

    chore(client): add pre-commit hook for linting, typecheck & formatting

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { configDefaults } from "vitest/dist/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: [...configDefaults.include, "./__tests__/*"],
+    include: ["./__tests__/*"],
     environment: "jsdom",
   },
 });
