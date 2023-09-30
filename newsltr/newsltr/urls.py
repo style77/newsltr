de9d14e0be751ce580ed70f1f90d5e2d68323e4a
style77 <stylek777@gmail.com>
2023-09-30 23:57:15 +0200

    chore(api): add swagger to urls

--- newsltr/newsltr/urls.py
+++ newsltr/newsltr/urls.py
@@ -9,31 +9,23 @@ urlpatterns = [
 ]
 
 if settings.DEVELOPMENT:
-    from drf_yasg.views import get_schema_view  # noqa
-    from drf_yasg import openapi  # noqa
-
-    schema_view = get_schema_view(
-        openapi.Info(
-            title="Your API",
-            default_version="v1",
-            description="Your API description",
-            terms_of_service="https://www.yourapp.com/terms/",
-            contact=openapi.Contact(email="contact@yourapp.com"),
-            license=openapi.License(name="Your License"),
-        ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-    )
+    from drf_spectacular.views import (
+        SpectacularAPIView,
+        SpectacularRedocView,
+        SpectacularSwaggerView,
+    )  # noqa
 
     urlpatterns += [
+        path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
         path(
-            "swagger/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
+            "api/v1/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
         ),
-        re_path(
-            r"^swagger(?P<format>\.json|\.yaml)$",
-            schema_view.without_ui(cache_timeout=0),
-            name="schema-json",
+        path(
+            "api/v1/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
         ),
     ]
