10661beaa473ed18eac10c516f6c171057d70f37
uKiJo <nes-you-ukijo@hotmail.fr>
2023-10-02 19:36:36 +0100

    feat(client): add register page

---
+++ client/components/ui/spinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Loader2 } from "lucide-react";
+
+const Spinner = () => {
+  return (
+    // <div className="animate-spin">
+    <Loader2 className="animate-spin" />
+    // </div>
+  );
+};
+
+export default Spinner;
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 // import counterReducer from "./features/counter/counterSlice";
 import authReducer from "./features/authSlice";
 import { apiSlice } from "./services/apiSlice";
@@ -9,6 +9,8 @@ const store = configureStore({
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
