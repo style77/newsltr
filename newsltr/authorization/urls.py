93c8be8e4bff058258af22a9ab678ac0a4821663
style77 <stylek777@gmail.com>
2023-10-15 02:10:01 +0200

    chore(api): save JWT tokens in httpOnly cookies

--- newsltr/authorization/urls.py
+++ newsltr/authorization/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 
 # Override the default djoser.urls.jwt and add my custom view for token obtain
-from authorization.views import EmailTokenObtainPairView
-from djoser.urls.jwt import urlpatterns as jwt_urlpatterns
-
-jwt_urlpatterns.pop(0)
+from authorization.views import EmailTokenObtainPairView, CustomTokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path(r"auth/", include("djoser.urls")),
-    path(r"auth/", include("djoser.urls.jwt")),
     path(r"auth/", include("djoser.social.urls")),
     path(r"auth/jwt/create/", EmailTokenObtainPairView.as_view(), name="jwt-create"),
+    path(r"auth/jwt/refresh/", CustomTokenRefreshView.as_view(), name="jwt-refresh"),
+    path(r"auth/jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify")
 ]
--- newsltr/newsltr/settings.py
+++ newsltr/newsltr/settings.py
@@ -133,7 +133,7 @@ AUTH_USER_MODEL = "authorization.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "authorization.authentication.JWTCookiesAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
@@ -158,7 +158,7 @@ if DEVELOPMENT:
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     # To include additional security let refresh_tokens be blacklisted
     # after using one of them
@@ -169,7 +169,19 @@ SIMPLE_JWT = {
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
     "TOKEN_OBTAIN_SERIALIZER": "authorization.serializers.CustomTokenObtainSerializer",
+    # "TOKEN_REFRESH_SERIALIZER": "authorization.serializers.CustomTokenRefreshSerializer",
     "USER_AUTHENTICATION_RULE": "authorization.utils.user_authentication_rule",
+
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+
+    "AUTH_COOKIE": "access_token",  # Cookie name. Enables cookies if value is set.
+    "REFRESH_COOKIE": "refresh_token",  # Refresh token cookie name. Enables cookies if value is set.
+    "AUTH_COOKIE_DOMAIN": None,  # A string like "example.com", or None for standard domain cookie.
+    "AUTH_COOKIE_SECURE": False,  # Whether the auth cookies should be secure (https:// only).
+    "AUTH_COOKIE_HTTP_ONLY": True,  # Http only cookie flag.It's not fetch by javascript.
+    "AUTH_COOKIE_PATH": "/",  # The path of the auth cookie.
+    "AUTH_COOKIE_SAMESITE": "Lax",  # Whether to set the flag restricting cookie leaks on cross-site requests.
+    # This can be 'Lax', 'Strict', or None to disable the flag.
 }
 
 DJOSER = {
