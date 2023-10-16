98522072ee496992b5473b8164e4e47902b8dc2a
style77 <stylek777@gmail.com>
2023-10-16 19:36:55 +0200

    test(api): basic tests

---
+++ newsltr/workspaces/tests/common.py
@@ -0,0 +1,30 @@
+from authorization.tests.common import create_user
+from workspaces.models import Workspace, WorkspaceMembership
+
+__all__ = [
+    "TEST_DATA",
+    "create_workspace",
+    "create_workspace_membership",
+]
+
+TEST_DATA = {
+    "name": "Test Workspace",
+    "description": "Test Workspace Description",
+}
+
+
+def create_workspace_membership(user, workspace, role="admin"):
+    return WorkspaceMembership.objects.create(user=user, workspace=workspace, role=role)
+
+
+def create_workspace(name=None, description=None, **kwargs):
+    data = {
+        "name": name or TEST_DATA["name"],
+        "description": description or TEST_DATA["description"],
+    }
+    data.update(kwargs)
+
+    workspace = Workspace.objects.create(**data)
+    admin = create_user()
+    create_workspace_membership(admin, workspace, role="admin")
+    return workspace
---
+++ newsltr/workspaces/tests/test_workspace_create.py
@@ -0,0 +1,48 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from djet import assertions
+
+from workspaces.models import Workspace, WorkspaceMembership
+from workspaces.tests.common import TEST_DATA
+
+from authorization.tests.common import (
+    TEST_DATA as TEST_USER_DATA,
+    create_user,
+    login_user,
+)
+
+
+class UserCreateViewTest(
+    APITestCase,
+    assertions.StatusCodeAssertionsMixin,
+    assertions.EmailAssertionsMixin,
+    assertions.InstanceAssertionsMixin,
+):
+    def setUp(self):
+        self.base_url = reverse("workspaces-list")
+
+    def test_post_create_workspace_without_authorization(self):
+        response = self.client.post(self.base_url, TEST_DATA)
+        self.assert_status_equal(response, status.HTTP_401_UNAUTHORIZED)
+        self.assertFalse(Workspace.objects.exists())
+
+    def test_post_create_workspace_with_authorization(self):
+        user = create_user()
+        login_user(self.client, TEST_USER_DATA["email"], TEST_USER_DATA["password"])
+
+        response = self.client.post(self.base_url, TEST_DATA)
+        self.assert_status_equal(response, status.HTTP_201_CREATED)
+        self.assertTrue(Workspace.objects.exists())
+
+        workspace = Workspace.objects.get(name=TEST_DATA["name"])
+        self.assertTrue(
+            WorkspaceMembership.objects.filter(
+                workspace=workspace, user=user
+            ).exists()
+        )
+        self.assertTrue(
+            WorkspaceMembership.objects.filter(
+                workspace=workspace, user=user, role="admin"
+            ).exists()
+        )
