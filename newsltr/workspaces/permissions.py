b7dff76be3ecaccf7f7fee3c66fe6a7a785ee886
style77 <stylek777@gmail.com>
2023-10-18 22:15:46 +0200

    chore(api): workspace api keys permissions

--- newsltr/workspaces/permissions.py
+++ newsltr/workspaces/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from .models import WorkspaceAPIKey
 
 
 class IsMemberOfWorkspace(permissions.BasePermission):
@@ -25,3 +26,46 @@ class IsModeratorOfWorkspace(permissions.BasePermission):
             ).exists()
             or request.user.is_superuser
         )
+
+
+class BearerKeyParser():
+    keyword = "Bearer"
+
+    def get(self, request):
+        return self.get_from_authorization(request)
+
+    def get_from_authorization(self, request):
+        authorization = request.META.get("HTTP_AUTHORIZATION")
+
+        if not authorization:
+            return None
+
+        try:
+            _, key = authorization.split("{} ".format(self.keyword))
+        except ValueError:
+            key = None
+
+        return key
+
+
+class HasWorkspaceAPIKey(permissions.BasePermission):
+    model = WorkspaceAPIKey
+    key_parser = BearerKeyParser()
+
+    def get_key(self, request):
+        return self.key_parser.get(request)
+
+    def has_permission(self, request, view):
+        assert self.model is not None, (
+            "%s must define `.model` with the API key model to use"
+            % self.__class__.__name__
+        )
+        key = self.get_key(request)
+        if not key:
+            return False
+        return self.model.objects.is_valid(key)
+
+    def has_object_permission(
+        self, request, view, obj
+    ):
+        return self.has_permission(request, view)
