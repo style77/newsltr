e0d2adccda117079e71487bc294368bf7c786bf3
uKiJo <nes-you-ukijo@hotmail.fr>
2023-10-21 23:39:00 +0100

    chore(client): add whatwg-fetch to solve tests on ci

--- client/vitest-setup.ts
+++ client/vitest-setup.ts
@@ -3,5 +3,4 @@ import "./app/globals.css";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: "./.env.local" });
-
-export const url = `${process.env.NEXT_PUBLIC_HOST}/api/v1/auth/`;
+import "whatwg-fetch";
