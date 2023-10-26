c4b51fedca389c138bb66963014849a951e133b1
style77 <stylek777@gmail.com>
2023-10-26 23:16:54 +0200

    style(api): fix linting and flake8 errors

--- newsltr/authorization/views.py
+++ newsltr/authorization/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from django.conf import settings
 
 from djoser.social.views import ProviderAuthView
-from social_django.utils import load_backend, load_strategy
 
 from rest_framework_simplejwt.views import (
     TokenViewBase,
@@ -36,16 +35,16 @@ from authorization.serializers import CustomTokenObtainPairSerializer, InActiveU
 )
 class CustomProviderAuthView(ProviderAuthView):
     @extend_schema(
-            parameters=[
-                OpenApiParameter(
-                    name="redirect_uri",
-                    required=False,
-                    location=OpenApiParameter.QUERY,
-                    type=OpenApiTypes.STR,
-                    default="http://127.0.0.1:3000/login",
-                    description="Redirect URI",
-                ),
-            ]
+        parameters=[
+            OpenApiParameter(
+                name="redirect_uri",
+                required=False,
+                location=OpenApiParameter.QUERY,
+                type=OpenApiTypes.STR,
+                default="http://127.0.0.1:3000/login",
+                description="Redirect URI",
+            ),
+        ]
     )
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
