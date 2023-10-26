a53a455c0671e0f68cb26f67929b2583e0d085e3
style77 <stylek777@gmail.com>
2023-10-26 17:13:15 +0200

    fix(api): typo

--- newsltr/payments/urls.py
+++ newsltr/payments/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('me/subscription/', views.Subscription.as_view()),
-    path('me/subscription/items/', views.SubscriptionItems.as_view()),
-    path('subsriptions/', views.SubscribableProductPrice.as_view()),
+    path('me/subscription/details/', views.SubscriptionItems.as_view()),
+    path('subscriptions/', views.SubscribableProductPrice.as_view()),
     path('checkout/', views.CreateStripeCheckoutSession.as_view()),
     path('webhook/', views.StripeWebhook.as_view()),
     path('customer-portal/', views.StripeCustomerPortal.as_view())
