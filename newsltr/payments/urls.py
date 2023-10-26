5bb3ae566e8bc6371c87110e00ea806b7accc670
style77 <stylek777@gmail.com>
2023-10-26 16:24:06 +0200

    refactor(api): payment endpoint urls

--- newsltr/payments/urls.py
+++ newsltr/payments/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-subscription/', views.Subscription.as_view()),
-    path('my-subscription-items/', views.SubscriptionItems.as_view()),
-    path('subscribable-product/', views.SubscribableProductPrice.as_view()),
+    path('me/subscription/', views.Subscription.as_view()),
+    path('me/subscription/items/', views.SubscriptionItems.as_view()),
+    path('subsriptions/', views.SubscribableProductPrice.as_view()),
     path('checkout/', views.CreateStripeCheckoutSession.as_view()),
     path('webhook/', views.StripeWebhook.as_view()),
     path('customer-portal/', views.StripeCustomerPortal.as_view())
