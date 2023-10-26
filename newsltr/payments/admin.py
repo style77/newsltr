3e3372d7286da1516aef8288be9ca9e904ca1020
style77 <stylek777@gmail.com>
2023-10-26 19:32:28 +0200

    chore(api): add payment models to admin site

--- newsltr/payments/admin.py
+++ newsltr/payments/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product, Price, Feature, ProductFeature, StripeUser
+from .models import (
+    Product,
+    Price,
+    Feature,
+    ProductFeature,
+    StripeUser,
+    SubscriptionItem,
+    Subscription
+)
 
 admin.site.register(Product)
 admin.site.register(Price)
 admin.site.register(Feature)
 admin.site.register(ProductFeature)
 admin.site.register(StripeUser)
+admin.site.register(SubscriptionItem)
+admin.site.register(Subscription)
