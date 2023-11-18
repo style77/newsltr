1402f2a1be2964694e6bb55a7eb2b780c70667fc
uKiJo <nes-you-ukijo@hotmail.fr>
2023-11-18 20:11:36 +0100

    feat(client): change the dashboard topnav to a route-based nav rather than a tab

---
+++ client/components/dashboard/TopnavLink.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface TopnavLinkProps {
+  href: string;
+  // children: React.ReactNode;
+  children: string;
+}
+
+const TopnavLink = ({ href, children }: TopnavLinkProps) => {
+  const pathname = usePathname();
+  const active = href === pathname;
+
+  return (
+    <li
+      className={`h-full border-b-4 ${
+        active ? " border-text font-bold" : "border-transparent"
+      }`}
+    >
+      <Link className="h-full align-middle flex items-center" href={href}>
+        {children}
+      </Link>
+    </li>
+  );
+};
+
+export default TopnavLink;
