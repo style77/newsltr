0408c54610cb5d7253dceff73175ec66a25391f3
kamczill <kam134@interia.eu>
2023-12-20 17:49:05 +0100

    feat: update grid styles in Features.jsx

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Hero />
       <Features />
+      <Pricing />
     </div>
   );
 }
